6b3f057165bff9a4067d1f54e38c0f79195ac1c0
BrunoCoelho <brunocoelho1997@gmail.com>
2019-03-06 12:42:40 +0000

    Can save and load trained networks

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,35 @@
+import pickle
+
 from src.mnist_loader import load_data_wrapper
 from src.network import Network
 
+FILEPATH = "trainedNetwork.pkl"
 
 def main():
-    file = open("testfile.txt", "w")
-    file.write("This is a test")
-    file.close()
 
+    net = None
+
+    try:
+        loadNetwork(net, FILEPATH)
+        print("Network loaded with success.")
+
+    except:
+        print("Did not found a previous network trained. Will train one.")
+        trainNetwork(net)
+        # sample usage
+        saveNetwork(net, FILEPATH)
+
+
+def saveNetwork(obj, filepath):
+    with open(filepath, 'wb') as output:  # Overwrites any existing file.
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+def loadNetwork(obj, filepath):
+    with open(filepath, 'rb') as input:
+        obj = pickle.load(input)
+
+def trainNetwork(net):
     training_data, validation_data, test_data = load_data_wrapper()
-
-
-
     net = Network([784, 30, 10])
     net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
